384b41bb22d15bf5d0e0c7d633bc3fac7fb24c5c
Bonora <albertob@qu-dvmcws-4808.corp.ctv.ca>
2019-11-05 09:20:33 -0500

    added dummy copy before album list.

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -8,6 +8,10 @@ class AlbumList extends React.Component {
               <div className="headerbar">somewhere in here will be a logo and maybe progress bar and will be sticky once page is longer</div>
               <div className="inner-wrapper">
                 {/* <h1>Hello {year ? year : " best albums"}</h1> */}
+                <div className="copy-wrapper">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum, lectus dignissim dapibus dignissim, velit sapien aliquam eros, ultricies rutrum nisi arcu quis dolor. Integer a eros consequat, commodo arcu at, pharetra nisl. Ut nec massa ut dui aliquet rhoncus. Nulla tristique mollis orci et accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc fringilla, justo at iaculis egestas, odio tellus congue turpis, eu eleifend mi ante at mauris. Mauris eu massa tortor. Nunc consectetur maximus vehicula. Suspendisse ultricies nisl tellus, id luctus nulla pulvinar vel. Mauris egestas iaculis auctor. Nullam tempus, elit mollis pellentesque fermentum, ex orci efficitur ex, vel pulvinar velit orci at risus. Vestibulum ac nibh in ligula porta condimentum.</p>
+                  <p>Duis lacinia ex lectus, eu eleifend tellus aliquam quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In posuere neque ex, vitae molestie ipsum maximus ut. Nunc vel molestie elit, nec lacinia justo. In vehicula, lorem consequat venenatis eleifend, est felis elementum felis, nec accumsan urna leo vel mauris. Duis tincidunt pharetra consectetur. Nunc eget eros arcu. Morbi vulputate sagittis ante et convallis. Ut ut ligula euismod, efficitur sapien quis, sollicitudin erat. Donec lacinia, nisi quis rutrum tempus, nisi sapien pellentesque libero, vitae sollicitudin dui turpis vel ex. Aliquam vel sapien nulla.</p>
+                </div>
                 <div className="albums-wrapper">
                   <div className="album">
                     <div className="album-details">
